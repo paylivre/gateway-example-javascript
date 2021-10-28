4a9535bdc28d5a52a5a04a47a5fd30c177115985
nei.jr.dev <n.junior@paylivre.com>
2021-10-28 11:29:48 -0300

    interno -> Adicionado salt randomico para gerar o hash do argon2i

--- src/services/argon2i.js
+++ src/services/argon2i.js
@@ -1,10 +1,12 @@
+import { getRandomHash } from "../utils/generatePropsRandom";
+
 const argon2 = require("argon2-browser");
 
 export async function getArgon2i(pass) {
   const argon2i = await argon2
     .hash({
       pass,
-      salt: "dZk8N6kUaA32XCsS",
+      salt: getRandomHash(14),
       time: 2,
       mem: 16,
       parallelism: 1,
--- src/utils/generatePropsRandom.js
+++ src/utils/generatePropsRandom.js
@@ -1,11 +1,14 @@
 import { v4 as uuidv4 } from "uuid";
 
-export function getRandomMerchantTransactionId() {
+export function getRandomHash(size) {
   const randomMerchantTransactionId = uuidv4().replace(/-/g, "");
-  return randomMerchantTransactionId.substring(0, 10);
+  return randomMerchantTransactionId.substring(0, size);
+}
+
+export function getRandomMerchantTransactionId() {
+  return getRandomHash(10);
 }
 
 export function getRandomUserAccountId() {
-  const randomMerchantTransactionId = uuidv4().replace(/-/g, "");
-  return randomMerchantTransactionId.substring(0, 6);
+  return getRandomHash(6);
 }
